fc52a798b2c4525397005d3fa71870b615c8d1d0
Yoan Fahrni <yoan.fahrni@divtec.ch>
2025-08-20 09:56:30 +0200

    Fini jalon 1

--- src/YahtzeeProcedural.java
+++ src/YahtzeeProcedural.java
@@ -1,7 +1,61 @@
+/*
+ * Description : Refaire le jeu du Yahtzee
+ * Auteur : Yoan Fahrni
+ * Date : 20.08.2025
+ */
 public class YahtzeeProcedural {
 
-    public static void main(String[] args) {
-        System.out.println( "Bon courage !");
+    /**
+     * Génère un nombre aléatoire entre 1 et 6 (Imite un lancer de dé)
+     * @return le nombre aléatoire génèré
+     */
+    public static int tirerUnDe() {
+
+
+        int nombreGenerer;
+        nombreGenerer = (int) (Math.random() * 6 + 1);
+        return nombreGenerer;
 
     }
+
+    /**
+     * Génère le nombre de fois voulu un nombre aléatoire avec l'aide de la fonction tirerUnDe
+     * @return un tableau avec les nombres généré
+     */
+    public static int[] nbreDe() {
+
+        //Créer un nouveau tableau
+        int[] tireGenerer = new int[5];
+
+        //Ajoute le nombre de lancé voulu dans le tableau
+        for (int i = 0; i < tireGenerer.length; i++) {
+            tireGenerer[i] = tirerUnDe();
+        }
+
+        return tireGenerer;
+    }
+
+    /**
+     * affiche une liste de nombre
+     * @param listeDeDes la liste qu'on veut afficher
+     */
+    public static void afficherDe(int[] listeDeDes) {
+
+        //affiche les dé un par un
+        for (int i = 0; i < listeDeDes.length; i++) {
+            System.out.println("Dé " + (i + 1) + " : " + listeDeDes[i]);
+        }
+
+    }
+
+    public static void main(String[] args) {
+
+        int[] lesFaces = new int[5];
+
+        lesFaces = nbreDe();
+
+        afficherDe(lesFaces);
+
+    }
+
 }
