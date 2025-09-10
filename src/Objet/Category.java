1bf91c30905a1b3668c138a83ee965e590d105fd
Yoan Fahrni <yoan.fahrni@divtec.ch>
2025-09-10 11:09:52 +0200

    création du type énuméré

--- src/Objet/Category.java
+++ src/Objet/Category.java
@@ -1,4 +1,19 @@
 package Objet;
 
 public class Category {
+
+    /**
+     * Les différentes combinaisons
+     */
+    enum Combinaisons {
+        PAIRE,
+        DOUBLE_PAIRE,
+        BRELAN,
+        CARRE,
+        FULL_HOUSE,
+        PETITE_SUITE,
+        GRANDE_SUITE,
+        YAHTZEE
+    }
+
 }
